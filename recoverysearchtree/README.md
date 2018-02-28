eebd0050ee719a7b99291fbce982b0e2d4d49f09
Mike Sutherland <mjfsutherland@gmail.com>
2018-02-28 10:54:36 -0800

    adding readmes to toy problem folders

---
+++ recoverysearchtree/README.md
@@ -0,0 +1,19 @@
+# Recovery Search Tree
+
+https://leetcode.com/problems/recover-binary-search-tree/description/
+
+# Description
+
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+
+# Note
+A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+# Failing Test Cases
+
+```test case: [2, null, 3, 1]
+output: [2, null, 3, 1]
+expected: [1, null, 3, 2]```
+
--- recoverysearchtree/recoversearchtree.js
+++ recoverysearchtree/recoversearchtree.js
@@ -1,21 +1,3 @@
-/* https://leetcode.com/problems/recover-binary-search-tree/description/ */
-
-/* Fails on
-test case: [2, null, 3, 1]
-output: [2, null, 3, 1]
-expected: [1, null, 3, 2]
-*/
-
-/*
-Two elements of a binary search tree (BST) are swapped by mistake.
-
-Recover the tree without changing its structure.
-
-Note:
-A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
-*/
-
-
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
